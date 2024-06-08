e658eaa69b60619374e0843d805fe6fbe7eafbfa
nobu12 <nobu12n@gmail.com>
2024-06-08 10:05:07 +0900

    chapter4

---
+++ chapter4/src/test/java/money/MoneyTest.java
@@ -0,0 +1,21 @@
+package money;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+public class MoneyTest {
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
+    }
+
+    @Test
+    public void testEquality() {
+        assertEquals(new Dollar(5), new Dollar(5));
+        assertNotEquals(new Dollar(5), new Dollar(6));
+    }
+}
